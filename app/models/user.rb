7bf210235b9b5754fc3e3bbb7d44796c80627873
taimoorshafiq27 <mtshafiq@outlook.com>
2023-12-14 14:31:23 +0000

    removed :registerable from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,5 +19,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
