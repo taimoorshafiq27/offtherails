16f7a88da32b1163925ce12baf56f3ee5c375587
taimoorshafiq27 <mtshafiq@outlook.com>
2023-12-15 12:47:44 +0000

    added blog post scopes

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,12 +2,36 @@
 #
 # Table name: blog_posts
 #
-#  id         :bigint           not null, primary key
-#  body       :text
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint           not null, primary key
+#  body         :text
+#  published_at :datetime
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 class BlogPost < ApplicationRecord
-    validates :title, :body, presence: true
+    validates :title, presence: true
+    validates :body, presence: true
+
+    # instead of fetching all blog posts when we do BlogPost.all, we want to define scopes to categorize posts with different statuses:
+    scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+    scope :draft, -> { where(published_at: nil) }
+    scope :published, -> { where("published_at <= ?", Time.current) }
+    scope :scheduled, -> { where("published_at >= ?", Time.current) }
+    # the lamda allows the app to evaluate Time.current every time the app asks for e.g. published_at scope posts, instead of there only being one Time.current which would be the Time.current when the app is booted up.
+
+    def draft?
+        published_at.nil?
+    end
+
+    def published?
+        published_at? && published_at <= Time.current
+    end
+
+    def schedule?
+        published_at? && published_at > Time.current
+    end
 end
+
+# add more validations
+# start adding tests
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -2,16 +2,19 @@
 #
 # Table name: blog_posts
 #
-#  id         :bigint           not null, primary key
-#  body       :text
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint           not null, primary key
+#  body         :text
+#  published_at :datetime
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "blog post is valid" do
+    blog_post = BlogPost.new(email: "email@hotmal.co.uk", body: "body")
+    blog_post.save
+    assert true
+  end
 end
